db947858ced84bcba385d889475dbd7587049e65
Mohammed Yacine Djenandar <62435906+yacine-djenandar@users.noreply.github.com>
2023-01-01 00:02:09 +0100

    Update new_messages.php

--- new_messages.php
+++ new_messages.php
@@ -1,7 +1,7 @@
 <div class="ch-container">
     <h1>New messages</h1>
     <?php
-        $pdo = new PDO('mysql:host=localhost;dbname=users', 'root', 'sonicmario1');
+        $pdo = new PDO('mysql:host=localhost;dbname=users', 'root', 'pwd');
         $query = $pdo->prepare("select * from message where receiver=? and isRead=false order by date");
         $query->bindValue(1, openssl_decrypt($_SESSION['token'], "AES-128-CTR", "11112000", 0, "1234567890123456"));
         $query->execute();
